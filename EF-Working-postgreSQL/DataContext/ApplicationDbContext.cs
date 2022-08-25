f5f1054d39ebc00deee4b3eefd6f103876e57301
ajinkyab <ajinkyab@AJINKYAB-PC>
2022-08-25 15:36:56 +0530

    Add project files.

---
+++ EF-Working-postgreSQL/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using EF_Working_postgreSQL.Models;
+namespace EF_Working_postgreSQL.DataContext
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext():base(nameOrConnectionString: "Myconnection")
+        { }
+
+
+        public virtual DbSet<EmpClass> Empobj { get; set; }
+    }
+}
